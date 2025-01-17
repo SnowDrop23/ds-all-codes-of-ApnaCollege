4f7fadcd212e358bc29c95b0bbb54fb270fee223
toma <144379705+SnowDrop23@users.noreply.github.com>
2025-01-17 11:35:17 +0600

    Create Pair Sum in Sorted Array(Two-pointer Approach).cpp
    Given sorted array with target
    Find such pair where there summation will be equal to target
    arr[i] + arr[j] = target, find i and j index

---
+++ Pair Sum in Sorted Array(Two-pointer Approach).cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> pairSum(vector<int> &nums, int target)
+{
+    vector<int>ans;
+    int i = 0, j = (nums.size()) - 1;
+
+    while(i < j)
+    {
+        int pairSum = nums[i] + nums[j];
+        if(pairSum > target) j--;
+        else if(pairSum < target) i++;
+        else
+        {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+    return ans;
+}
+
+
+int main ()
+{
+    int n, target;
+    cout << "Enter size of array: ";
+    cin >> n;
+    cout << "Enter target value: ";
+    cin >> target;
+    vector<int>nums(n);
+    for(int i = 0; i < n; i++) cin >> nums[i];
+    vector<int>ans = pairSum(nums, target);
+
+    if(ans.empty()) cout << "No such pair exits." << endl;
+    else cout << "(" << ans[0] << ", " << ans[1] << ")" << endl;
+    
+    return 0;
+    
+}
