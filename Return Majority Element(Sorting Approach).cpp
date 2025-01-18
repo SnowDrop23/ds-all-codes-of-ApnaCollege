e8cc87ce8cdd42889f889f4985f42bc31bc37853
toma <144379705+SnowDrop23@users.noreply.github.com>
2025-01-18 10:09:04 +0600

    Create Return Majority Element(Sorting Approach).cpp

---
+++ Return Majority Element(Sorting Approach).cpp
@@ -0,0 +1,36 @@
+//Question link: https://leetcode.com/problems/majority-element/description/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int majorityElement(vector<int>&nums)
+{
+	int n = nums.size(), count = 1;
+	for(int i = 1; i < n; i++)
+	{
+		if(nums[i] == nums[i-1]) count++;
+		else 
+		{
+			if(count > n/2) return nums[i];
+			else count = 1;
+		}
+	}
+	if(count > n/2) return nums[n-1]; // The majority element is in the last segment
+	else return -1;
+	
+	
+}
+int main ()
+{
+   	int n;
+	cin >> n;
+	vector<int> nums(n);
+	for(int i = 0; i < n; i++) cin >> nums[i];
+	sort(nums.begin(), nums.end());
+	int ans = majorityElement(nums);
+	if(ans != -1) cout << ans << endl;
+	else cout << "Majority element not found!";
+	
+
+}
