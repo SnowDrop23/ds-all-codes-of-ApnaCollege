0a758e0ef8ccaa273acd6ee368ede893bcc9ce5e
toma <144379705+SnowDrop23@users.noreply.github.com>
2025-01-28 22:17:20 +0600

    Create Pow(X,N) Power exponential Problem.cpp

---
+++ Pow(X,N) Power exponential Problem.cpp
@@ -0,0 +1,33 @@
+//Question Link: https://leetcode.com/problems/powx-n/description/
+#include<iostream>
+using namespace std;
+
+double(myPow(double x, int n))
+{
+    long binForm = n;
+    double ans = 1;
+
+    while(binForm > 0)
+    {
+        if(binForm % 2 == 1) ans *= x;
+        x *= x;
+        
+        binForm /= 2;
+    }
+    cout << endl;
+    return ans;
+}
+
+
+int main ()
+{
+    int n; //power
+    double x; //base
+    cout << "Enter power and base: ";
+    cin >> n >> x;
+    double ans = myPow(x, n);
+    cout << ans << endl;
+    
+    return 0;
+    
+}
