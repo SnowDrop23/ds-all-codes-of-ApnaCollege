fbd0a278429e69f3b8093b21e52be169cf91a7a1
toma <144379705+SnowDrop23@users.noreply.github.com>
2025-03-23 22:42:42 +0600

    Create place all 0s to left and all 1s to right.cpp

---
+++ 0s to left and 1s to right.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void sortColors(vector<int>&nums)
+{
+    int n = nums.size();
+    int low = 0, high = n-1;
+
+    while(low < high)
+    {
+        if(nums[low] == 1)
+        {
+            swap(nums[low], nums[high]);
+            high--;
+        }
+        else low++;
+        
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+    vector<int>arr(n);
+    
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sortColors(arr);
+    for(int i = 0; i < n; i++) cout << arr[i] << " ";
+    return 0;
+}
