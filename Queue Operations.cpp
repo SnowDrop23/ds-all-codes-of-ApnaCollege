29b7a1ef148eab1f73c8468b265e6932ec7ca61e
toma <144379705+SnowDrop23@users.noreply.github.com>
2025-04-07 13:52:35 +0600

    Update Queue Operations.cpp

--- Queue Operations.cpp
+++ Queue Operations.cpp
@@ -16,9 +16,15 @@ void display(queue<int>q)
 
 int main()
 {
+    int x;
 
     queue<int>q;
-    for(int i = 0; i < 5; i++) q.push(i+1);
+    cout << "Enter queue elements: ";
+    for(int i = 0; i < 5; i++) 
+    {
+        cin >> x;
+        q.push(x);
+    }
 
     cout << "Size of the queue is " << q.size() << endl;
     cout << "The elements of queue are: ";
@@ -30,11 +36,16 @@ int main()
 
     if(!q.empty()) cout << "Queue is not empty" << endl;
     
-    q.pop();
+    cout << q.front() << " popped from queue." << endl;
     q.pop();
 
+    cout << q.front() << " popped from queue." << endl;
+    q.pop();
+
+    cout << "The elements of the queue are: ";
     display(q);
     q.push(80);
+    cout << q.back() << " pushed in the queue." << endl;
 
     cout << "Size of the queue: " << q.size() << endl;
     cout << "The elements of the queue are: ";
